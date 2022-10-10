f842b855730c1129db04ed41302819cca4dd56fd
ZhengChangBing <zhengchangbing@rd.keytop.com.cn>
2022-10-10 18:15:29 +0800

    mybatis-plus配置

--- src/main/java/cn/day/test/TestApplication.java
+++ src/main/java/cn/day/test/TestApplication.java
@@ -1,9 +1,11 @@
 package cn.day.test;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.day.test.mapper")
 public class TestApplication {
 
     public static void main(String[] args) {
